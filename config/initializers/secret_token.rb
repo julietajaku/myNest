6d50bc3d018c2082e59fbd3be3efa0fedb9a09e4
La cholita <julietajaku@gmail.com>
2014-07-01 11:09:41 +0200

    Basic spree store
    http://guides.spreecommerce.com/developer/getting_started_tutorial.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyNest::Application.config.secret_key_base = '9bdb058fc1bc432e1778dd171f0c8a507aeb7f3635c62b4e35bc03de5fff688827920f7c246256e36a81d5d6274d599c59c05cb634c9e39b3a997a4001128820'
